6cc4fdd2789447f8c1e168444e66cfd4a55f28aa
Niklas Muennighoff <niklasmuennighoff@Niklass-MBP.home>
2021-06-18 16:47:17 +0200

    Clarify credential loading

--- python-lib/amazon_translation_api_client.py
+++ python-lib/amazon_translation_api_client.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """Module with utility functions to call the Google translation API"""
 
-import json
 import logging
 
 import boto3
-from botocore.exceptions import ClientError
-from botocore.exceptions import BotoCoreError
 from botocore.config import Config
+from botocore.exceptions import BotoCoreError
+from botocore.exceptions import ClientError
+from botocore.exceptions import NoRegionError
 
 # ==============================================================================
 # CONSTANT DEFINITION
@@ -23,32 +23,46 @@ API_EXCEPTIONS = (ClientError, BotoCoreError)
 
 
 def get_client(
-            aws_access_key_id=None, 
-            aws_secret_access_key=None, 
-            aws_session_token=None, 
-            aws_region_name=None, 
-            max_attempts=20
-            ):
+    aws_access_key_id=None,
+    aws_secret_access_key=None,
+    aws_session_token=None,
+    aws_region_name=None,
+    max_attempts=20,
+):
     """
-    Get a translate API client from the AWS credentials.
+    Gets a translation API client from AWS credentials.
     """
     # Try to ascertain credentials from environment
     if aws_access_key_id is None or aws_access_key_id == "":
         logging.info("Attempting to load credentials from environment.")
-        return boto3.client(service_name='translate')
-    
-    try:
-        client = boto3.client(
-            service_name='translate', 
-            aws_access_key_id=aws_access_key_id, 
-            aws_secret_access_key=aws_secret_access_key, 
-            aws_session_token=aws_session_token, 
-            region_name=aws_region_name, 
-            config=Config(retries={'max_attempts': max_attempts})
-        )
-    except ClientError as e:
-        logging.error(e)
-        raise
+        try:
+            client = boto3.client(
+                service_name="translate", config=Config(retries={"max_attempts": max_attempts})
+            )
+        except NoRegionError as e:
+            logging.info(
+                "The region could not be loaded from the environment. "
+                "If you have specified the region in the environment, "
+                "DSS may not be able to access the environemt, "
+                "such as in a User Isolation Framework setup. "
+                "You may have to specify credentials in the plugin configuration."
+            )
+            logging.error(e)
+            raise
+    # Use configured credentials
+    else:
+        try:
+            client = boto3.client(
+                service_name="translate",
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=aws_session_token,
+                region_name=aws_region_name,
+                config=Config(retries={"max_attempts": max_attempts}),
+            )
+        except ClientError as e:
+            logging.error(e)
+            raise
 
-    logging.info("Credentials loaded")
+    logging.info("Credentials loaded.")
     return client
