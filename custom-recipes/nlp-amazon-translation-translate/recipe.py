71c7283ee050e0fa1efc8725c37b544beff6bdda
Niklas Muennighoff <niklasmuennighoff@Niklass-MBP.home>
2021-07-15 20:14:01 +0200

    Use submodule

--- custom-recipes/nlp-amazon-translation-translate/recipe.py
+++ custom-recipes/nlp-amazon-translation-translate/recipe.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import importlib
 import json
 from typing import Dict
 from typing import AnyStr
@@ -10,10 +11,12 @@ from dataiku.customrecipe import get_output_names_for_role
 
 from amazon_translation_api_client import API_EXCEPTIONS, get_client
 from amazon_translation_api_formatting import TranslationAPIFormatter
-from api_parallelizer import api_parallelizer
-from dku_io_utils import set_column_description
+
 from plugin_io_utils import ErrorHandlingEnum
 from plugin_io_utils import validate_column_input
+
+parallelizer = importlib.import_module("dss-plugin-dkulib.dkulib.parallelizer.parallelizer", None)
+dku_io_utils = importlib.import_module("dss-plugin-dkulib.dkulib.dku_io_utils", None)
 
 # ==============================================================================
 # SETUP
@@ -83,22 +86,28 @@ formatter = TranslationAPIFormatter(
 # RUN
 # ==============================================================================
 
-df = api_parallelizer(
-    input_df=input_df,
-    api_call_function=call_translation_api,
-    api_exceptions=API_EXCEPTIONS,
-    column_prefix=column_prefix,
-    parallel_workers=parallel_workers,
+
+df_parallelizer = parallelizer.DataFrameParallelizer(
+    function=call_translation_api,
     error_handling=error_handling,
+    exceptions_to_catch=API_EXCEPTIONS,
+    parallel_workers=parallel_workers,
+    batch_size=1,
+    output_column_prefix=column_prefix,
+)
+
+df = df_parallelizer.run(
+    input_df,
     text_column=text_column,
     target_language=target_language,
     source_language=source_language,
 )
+
 output_df = formatter.format_df(df)
 output_dataset.write_with_schema(output_df)
 
-set_column_description(
+dku_io_utils.set_column_descriptions(
     input_dataset=input_dataset,
     output_dataset=output_dataset,
-    column_description_dict=formatter.column_description_dict,
+    column_descriptions=formatter.column_description_dict,
 )
